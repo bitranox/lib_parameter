efbd764656eb78a96afb422fb4e52b9731f8df85
bitranox <1uGToNSiYo85pWhQcqFu>
2019-07-22 18:52:50 +0200

    refractor

---
+++ lib_parameter/__init__.py
@@ -0,0 +1,5 @@
+from .lib_parameter import *
+
+__title__ = 'lib_parameter'
+__version__ = '0.0.1'
+__name__ = 'lib_parameter'
---
+++ lib_parameter/lib_parameter.py
@@ -0,0 +1,6 @@
+
+def get_default_if_none(parameter, default):
+    if parameter is None:
+        return default
+    else:
+        return parameter
