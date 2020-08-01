a5d009124b779993d1ce51b4efe2dd647e8842c1
bitranox <rnowotny1966@gmail.com>
2020-08-01 16:00:27 +0200

    refractor

--- lib_parameter/__init__conf__.py
+++ lib_parameter/__init__conf__.py
@@ -1,6 +1,6 @@
 name = 'lib_parameter'
 title = 'small gist,to return a default value if the parameter is None'
-version = '1.1.4'
+version = 'v1.1.5'
 url = 'https://github.com/bitranox/lib_parameter'
 author = 'Robert Nowotny'
 author_email = 'bitranox@gmail.com'
@@ -14,7 +14,7 @@ Info for lib_parameter:
 
     small gist,to return a default value if the parameter is None
 
-    Version : 1.1.4
+    Version : v1.1.5
     Url     : https://github.com/bitranox/lib_parameter
     Author  : Robert Nowotny
     Email   : bitranox@gmail.com""")
